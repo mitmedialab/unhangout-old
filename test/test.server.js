ba42cd63d0442a78285eee101be29676456029b4
Drew Harry <drew.harry@gmail.com>
2013-06-04 16:31:41 -0400

    Added requirement for google credentials, and test for their absence.

--- test/test.server.js
+++ test/test.server.js
@@ -4,22 +4,43 @@ var server = require('../lib/unhangout-server'),
 
 var s;
 
+var standardSetup = function() {
+	s = new server.UnhangoutServer();
+	s.init({"transport":"file", "level":"debug", "GOOGLE_CLIENT_ID":true, "GOOGLE_CLIENT_SECRET":true});
+	s.start();
+}
+
+var standardShutdown = function(done) {
+	s.stop();
+	s.on("stopped", function() {
+		s.on("destroyed", done);
+		s.destroy();
+	})
+}
+
 describe('unhangout server', function() {
-	beforeEach(function() {
-		s = new server.UnhangoutServer();
-		s.init({"transport":"file", "level":"debug"});
-	});
-	
-	afterEach(function(done) {
-		s.stop();
-		s.on("stopped", function() {
-			s.on("destroyed", done);
-			s.destroy();
-		})
+	describe('configuration', function() {
+		it('should not initialize without google credentials', function(done) {
+			s = new server.UnhangoutServer();
+			s.on("error", function() {
+				done();
+			});
+			s.on("inited", function() {
+				should.fail("Expected an error.");
+			});
+			s.init({"transport":"file", "level":"debug"});
+		});	
 	});
 	
 	
 	describe('setup', function() {
+		beforeEach(function() {
+			s = new server.UnhangoutServer();
+			s.init({"transport":"file", "level":"debug", "GOOGLE_CLIENT_ID":true, "GOOGLE_CLIENT_SECRET":true});
+		});
+
+		afterEach(standardShutdown);
+		
 		it('#start should start', function() {
 			s.start();
 		});
@@ -33,9 +54,10 @@ describe('unhangout server', function() {
 	
 	describe('routes', function() {
 		
-		beforeEach(function() {
-			s.start();
-		});
+		beforeEach(standardSetup);
+		
+		afterEach(standardShutdown);
+		
 		
 		describe("GET /", function() {
 			it('should return without error', function(done) {
