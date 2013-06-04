88092b65a78b02b89e7a7290f7721bd276383c5a
Drew Harry <drew.harry@gmail.com>
2013-06-04 10:54:08 -0400

    Wrote some trivial tests.

--- test/test.server.js
+++ test/test.server.js
@@ -1,3 +1,20 @@
-describe('server', function() {
-	it('should instantiate');
+var server = require('../lib/unhangout-server');
+
+var s;
+
+describe('unhangout server', function() {
+	beforeEach(function() {
+		s = new server.UnhangoutServer();
+	});
+	
+	describe('setup', function() {
+		it('#init should initialize', function() {
+			s.init();
+		});
+
+		it('#start should start', function() {
+			s.init();
+			s.start();
+		});
+	});
 })
