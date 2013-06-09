b2923f0acd2166dc49800c67e88178dd66f8209d
Drew Harry <drew.harry@gmail.com>
2013-06-09 16:41:05 -0400

    Testing reading with sync.

--- test/test.sync.js
+++ test/test.sync.js
@@ -15,6 +15,7 @@ var logger= new (winston.Logger)({
     levels: winston.config.syslog.levels
 });
 
+var event;
 
 
 describe('sync', function() {
@@ -37,25 +38,45 @@ describe('sync', function() {
 		});
 
 		it('should assign an id to an object that doesn\'t have one.', function(done) {
-			var user = new models.Event();
-			user.save(null, {success: function() {
-				user.get("id").should.equal(1);
+			event = new models.Event();
+			event.save(null, {success: function() {
+				event.get("id").should.equal(1);
 				done();
 			}});
 		});
 		
 		it('should write JSON to a key specified by the object\'s url.', function(done) {
-			var user = new models.Event();
-			user.save(null, {success: function() {
-				redis.get(user.url(), function(err, res) {
-					res.should.equal(JSON.stringify(user.toJSON()));
+			event = new models.Event();
+			event.save(null, {success: function() {
+				redis.get(event.url(), function(err, res) {
+					res.should.equal(JSON.stringify(event.toJSON()));
 					done();
 				});
 			}});
 		});
+
+		describe('.read (model) ', function() {
+			beforeEach(function(done) {
+				event = new models.Event();
+				event.save(null, {success: function() {
+					done()}});
+			});
+			
+			it('should fetch a model properly', function(done) {
+				var emptyEvent = new models.Event({id:1, title: "asdf"});
+				
+				emptyEvent.fetch({success: function(model, response, options) {
+					model.get("title").should.equal(event.get("title"));
+					done();
+				}});
+			});
+			
+			it('should update the contents of a collection');
+			it('should remove an item from a collection');
+		});
 		
 		
-		describe('.read', function() {
+		describe('.read (collection) ', function() {
 			it('should fetch a collection properly');
 			it('should update the contents of a collection');
 			it('should remove an item from a collection');
