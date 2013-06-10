a31cea3987c1e814657cdf87583f4f986ca14c14
Drew Harry <drew.harry@gmail.com>
2013-06-09 20:57:24 -0400

    Sock connection and default connection handlers are up and running on both sides.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
+var sock;
+
 $(document).ready(function() {
 	if($("#app").length!=1) {
 		console.log("Code running on a page that does not have an #app div.");
 		return;
 	}
-	
+
 	console.log("Starting app!");
+
+	sock = new SockJS(document.location.protocol + "//" + document.location.hostname + ":" + document.location.port + "/sock");
+	sock.onopen = function() {
+		console.log('open');
+	};
+	sock.onmessage = function(e) {
+		console.log('message', e.data);
+	};
+	sock.onclose = function() {
+		console.log('close');
+	};
 });
