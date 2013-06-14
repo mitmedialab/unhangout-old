8d88bc8118e1c4f09193ae00823399c3c399344d
Drew Harry <drew.harry@gmail.com>
2013-06-14 13:57:37 -0400

    First pass at getting server models up and running. Passing tests.

--- public/js/models.js
+++ public/js/models.js
@@ -109,43 +109,10 @@ exports.SessionList = Backbone.Collection.extend({
 	},
 });
 
-exports.USER_KEY_SALT = "SET ME EXTERNALLY";
 
 exports.User = Backbone.Model.extend({
 	idRoot: "user",
-	urlRoot: "user",
-	
-	// This method generates time invariant key that gets embedded in all pages
-	// and can be used on the sockjs channel to authenticate a sock connection
-	// as belonging to this user. It is simply the id of the user plus some salt.
-	// The user can then present this key plus the userid they wish to authenticate
-	// as, and the server can verify that it matches the key it would have identified
-	// using that salt.
-	getSockKey: function() {
-		if(_.isUndefined(this.get("sock-key"))) {
-			var shasum = crypto.createHash('sha256');
-			shasum.update(this.get("id"));
-			shasum.update(exports.USER_KEY_SALT);
-			this.set("sock-key", shasum.digest('hex'));
-		}
-		
-		return this.get("sock-key");
-	},
-	
-	validateSockKey: function(key) {
-		return key == this.getSockKey();
-	},
-	
-	isConnected: function() {
-		return !_.isUndefined(this.get("sock")) && !_.isNull(this.get("sock"));
-	},
-	
-	toJSON: function() {
-		var attrs = _.clone(this.attributes);
-		delete attrs["sock-key"];
-		delete attrs["sock"];
-		return attrs;
-	}
+	urlRoot: "user"
 });
 
 exports.UserList = Backbone.Collection.extend({
