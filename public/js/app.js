be8dc8a732020b8c51cfb8f9c5adde8e439d9440
Drew Harry <drew.harry@gmail.com>
2013-06-14 17:57:50 -0400

    Added event data into the event page, some bits and pieces of message handling on the client too.

--- public/js/app.js
+++ public/js/app.js
@@ -16,9 +16,33 @@ $(document).ready(function() {
 		
 		sock.send(JSON.stringify(AUTH));
 	};
-	sock.onmessage = function(e) {
-		console.log('message', e.data);
+	sock.onmessage = function(message) {
+		console.log(message);
+		var msg = JSON.parse(message.data);
+		
+		if(msg.type.indexOf("-err")!=-1) {
+			console.log("Got an error from the server!");
+		}
+		
+		switch(msg.type) {
+			case "attend":
+				console.log("attend: " + JSON.stringify(msg.args));
+				break;
+				
+			case "join":
+				console.log("join: " + JSON.stringify(msg.args));
+				break;
+				
+			case "auth-ack":
+				sock.send(JSON.stringify({type:"join", args:{id:EVENT_ID}}));
+				break;
+				
+			case "join-ack":
+				console.log("joined!");
+				break;
+		}
 	};
+
 	sock.onclose = function() {
 		console.log('close');
 	};
