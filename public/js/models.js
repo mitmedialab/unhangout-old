24ddad55ce326ebc8d00e19323128e263575e68f
Drew Harry <drew.harry@gmail.com>
2013-06-25 11:41:44 -0400

    Changed how we extract the picture field from JSON to deal with lazy loading.

--- public/js/models.js
+++ public/js/models.js
@@ -156,15 +156,17 @@ models.SessionList = Backbone.Collection.extend({
 
 models.User = Backbone.Model.extend({
 	initialize: function() {
-		// copy some bonus fields out of the attributes if present.
-		if("_json" in this.attributes) {
-			var json = this.get("_json");
-			
-			if("picture" in json) { this.set("picture", this.get("_json").picture); }
-			else { this.set("picture", "")}
-			
-			if("link" in json) this.set("link", this.get("_json").link);
-		}
+		this.on("change:_json", function(model, name) {
+			// copy some bonus fields out of the attributes if present.
+			if("_json" in this.attributes) {
+				var json = this.get("_json");
+
+				if("picture" in json) { this.set("picture", this.get("_json").picture); }
+				else { this.set("picture", "")}
+
+				if("link" in json) this.set("link", this.get("_json").link);
+			}
+		})
 	}
 });
 
