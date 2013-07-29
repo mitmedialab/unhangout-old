834216c09114f0afa37ab00af2e9b0be007a9106
Drew Harry <drew.harry@gmail.com>
2013-07-29 15:53:25 -0400

    Added a small script to get all user emails out of a redis database with user objects in it.

---
+++ bin/get-all-user-emails.js
@@ -0,0 +1,36 @@
+var _ = require('underscore')._,
+	async = require('async'),
+	redis = require('redis');
+
+if(require.main === module) 
+{	
+	var r = redis.createClient();
+	r.on("connect", function() {
+
+		console.log("connected to redis");
+
+		r.keys("user*", function(err, res) {
+			async.series(_.map(res, function(key) {
+				return function(cb) {
+					console.log("running get on key: " + key);
+					r.get(key, function(err, res) {
+						var user = JSON.parse(res);
+						cb(null, user.emails);
+					});
+				};
+			}), function(err, results) {
+
+				var emails = [];
+
+				_.each(results, function(list) {
+					_.each(list, function(item) {
+						emails.push(item["value"]);
+						console.log(item["value"]);
+					});
+				});
+
+				process.exit();
+			});
+		});
+	});
+}
