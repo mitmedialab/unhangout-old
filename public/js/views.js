815745740818c8ffd470ad0d52a927763f5f71cc
SrishAkaTux <srishakatux@gmail.com>
2013-08-01 16:24:11 -0400

    Modified the regular expression in linkify function to support email addresses that contain multiple dots before or after @ symbol

--- public/js/views.js
+++ public/js/views.js
@@ -365,19 +365,19 @@ var ChatMessageView = Marionette.ItemView.extend({
 	},
 
 	linkify: function(msg) {
-		var replacedText, replacePattern1, replacePattern2, replacePattern3;
+		var replacedText, replacePattern1, replacePattern2, replacePattern3, replacePattern4;
 
     	//URLs starting with http://, https://, or ftp://
-    	replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    	replacedText = msg.replace(replacePattern1, "<a href='$1' target='_new'>$1</a>");
+     	replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+     	replacedText = msg.replace(replacePattern1, "<a href='$1' target='_new'>$1</a>");
 
-    	//URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    	replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    	replacedText = replacedText.replace(replacePattern2, "$1<a href='http://$2' target='_new'>$2</a>");
+     	//URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+     	replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+     	replacedText = replacedText.replace(replacePattern2, "$1<a href='http://$2' target='_new'>$2</a>");
 
-    	//Change email addresses to mailto:: links.
-    	replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
-		replacedText = replacedText.replace(replacePattern3, "<a href='mailto:$1'>$1</a>");
+     	//Change email addresses to mailto:: links.
+     	replacePattern3 = /(([a-zA-Z0-9\-?\.?]+)@(([a-zA-Z0-9\-_]+\.)+)([a-z]{2,3}))+$/;
+	    replacedText = replacedText.replace(replacePattern3, "<a href='mailto:$1'>$1</a>");
 
     	return replacedText;
 	},
