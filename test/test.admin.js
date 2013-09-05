3d93457f3806b37599bb1d5574a0f1e56da9b0d6
Drew Harry <drew.harry@gmail.com>
2013-09-05 09:44:36 -0400

    Wrote the remaining tests. Board is green.

--- test/test.admin.js
+++ test/test.admin.js
@@ -47,7 +47,7 @@ describe('HTTP ADMIN API', function() {
 				.redirects(0)
 				.end(function(res) {
 					res.status.should.equal(302);
-					// res.header['location'].should.equal("/auth/google");
+					res.header['location'].should.equal("/");
 					done();
 				});
 		});
@@ -94,9 +94,45 @@ describe('HTTP ADMIN API', function() {
 		});
 	});
 
-	describe('/admin/event/:id', function() {
-		it('should reject well-formed requests from non-admins');
-		it('should accept well-formed creation request from admin');
-		it('should redirect to /admin/event/:id on successful creation');
+	describe('/admin/event/:id (non-admin)', function() {
+		beforeEach(mockSetup(false));
+
+		it('should reject well-formed requests from non-admins', function(done) {
+			request.post('http://localhost:7777/admin/event/1')
+				.send({title:"Test Event", description:"Description of the test event."})
+				.redirects(0)
+				.end(function(res) {
+					res.status.should.equal(302);
+					res.header['location'].should.equal("/");
+					done();
+				});
+		});
+	});
+
+	describe('/admin/event/:id (admin)', function() {
+		beforeEach(mockSetup(true));
+
+		it('should accept well-formed creation request from admin', function(done) {
+			request.post('http://localhost:7777/admin/event/1')
+				.send({title:"Test Event", description:"Description of the test event."})
+				.redirects(0)
+				.end(function(res) {
+					res.status.should.equal(302);
+					s.events.at(0).get("title").should.equal("Test Event");
+					s.events.at(0).get("description").should.equal("Description of the test event.");
+					done();
+				});
+		});
+
+		it('should redirect to /admin/event/:id on successful creation', function(done) {
+			request.post('http://localhost:7777/admin/event/1')
+				.send({title:"Test Event", description:"Description of the test event."})
+				.redirects(0)
+				.end(function(res) {
+					res.status.should.equal(302);
+					res.header['location'].should.equal("/admin/event/1");
+					done();
+				});
+		});
 	});
 });
