825e9837d01022cef9daf8e1a4fc5e4503976421
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2013-09-10 12:06:17 -0400

    three tests working for the permalink branch.  hooray.

--- test/test.server.js
+++ test/test.server.js
@@ -216,7 +216,7 @@ describe('unhangout server', function() {
 		beforeEach(mockSetup(false));
 		afterEach(standardShutdown);
 
-		xit('should direct to the landing page when there is no code', function(done){
+		it('should direct to the landing page when there is no code', function(done){
 			request.get('http://localhost:7777/h/')
 				.end(function(res){
 					res.status.should.equal(200);
@@ -224,62 +224,59 @@ describe('unhangout server', function() {
 				});
 		});
 
-		xit('if :code is new, it should create a new session on the server', function(done){
-			request.get('http://localhost:7777/h/test', function(){
+		it('if :code is new, it should create a new session on the server', function(done){
+			request.get('http://localhost:7777/h/test')
+				.redirects(0)
 				.end(function(res){
-					res.status.should.equal(304);
+					res.status.should.equal(302);
+					s.permalinkSessions.length.should.equal(1);
+					done();
 				});
-			});
-			s.permalinkSession.should.equal(1);
-			done();
-
 		});
 
 		it('if :code is active, multiple requests only create one session', function(done){
-			request.get('http://localhost:7777/h/test', function(){
+			request.get('http://localhost:7777/h/test')
+				.redirects(0)
 				.end(function(res){
-					res.status.should.equal(304);
-					s.permalinkSession.should.equal(1);
-					request.get('http://localhost:7777/h/test', function(){
+					res.status.should.equal(302);
+					s.permalinkSessions.length.should.equal(1);
+					request.get('http://localhost:7777/h/test')
 						.end(function(res){
 							res.status.should.equal(200);
-							s.permalinkSession.should.equal(1);
+							s.permalinkSessions.length.should.equal(1);
 							done();
 						});
-					});
 				});
-			});
 		});
 
 		xit('if :code is new, it should present the form only for first visitor', function(done){
-			request.get('http://localhost:7777/h/test', function(){
+			request.get('http://localhost:7777/h/test')
 				.end(function(res){
 					res.body.indexOf(/*form field*/).should.not.equal(-1);
 				});
-			});
-			request.get('http://localhost:7777/h/test', function(){
+			request.get('http://localhost:7777/h/test')
 				.end(function(res){
 					res.body.indexOf(/*form field*/).should.equal(-1);
 				});
-			});
 			done();
 		});
 
 	});
 
-	describe('POST /h/:code', function(){
-		beforeEach(mockSetup(false, function(done){
-			request.get('http://localhost:7777/h/test')
-				.end(function(res) {
-					res.status.should.equal(200);
-			done();
-		}));
+	// describe('POST /h/:code', function(){
+	// 	beforeEach(mockSetup(false, function(done){
+	// 		request.get('http://localhost:7777/h/test')
+	// 			.end(function(res) {
+	// 				res.status.should.equal(200);
+	// 			done();
+	// 		});
+	// 	}));
 
-		afterEach(standardShutdown);
+	// 	afterEach(standardShutdown);
 
-		xit('shou')
+	// 	xit('shou')
 
-	});
+	// });
 
 	describe('POST /session/hangout/:id', function() {
 		beforeEach(mockSetup(false, function(done) {
