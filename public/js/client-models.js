5b6d37a65948804f2322deb395e0dda7a06cc481
Drew Harry <drew.harry@gmail.com>
2013-11-13 23:51:32 -0500

    Removed the sorting events entirely, rather than just not sorting.

--- public/js/client-models.js
+++ public/js/client-models.js
@@ -7,12 +7,12 @@ models.ClientSessionList = models.SessionList.extend({
 	initialize: function(options) {
 		models.SessionList.prototype.initialize.call(this, options);
 
-		this.on("add", _.bind(function(session) {
-			session.on("change:attendeeIds change:connectedParticipantIds", _.bind(function() {
-				// disabling sorting for now.
-				// this.sort();
-			}, this));
-		}, this));
+		// disabling sorting for now.
+		// this.on("add", _.bind(function(session) {
+		// 	session.on("change:attendeeIds change:connectedParticipantIds", _.bind(function() {
+		// 		this.sort();
+		// 	}, this));
+		// }, this));
 	},
 
 	comparator: function(a, b) {
