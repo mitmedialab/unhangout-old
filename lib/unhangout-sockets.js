mitmedialab/unhangout-old

commit bc96b1350780f5991b1326bd0a1a5cf15a82af62

Fix order-of-operations for hoa/event setting
Issue #330.

lib/unhangout-sockets.js
@@ -336,21 +336,21 @@ _.extend(UnhangoutSocketManager.prototype, events.EventEmitter.prototype, {
         mgr.on("session/set-hangout-broadcast-id", function(socket, args) {
             // Hangouts on air, setting the broadcast ID for their youtube feed.
             var route = "session/set-hangout-broadcast-id";
-            var session = this.ensureSocketInSession(mgr, socket, args.sessionId, route);
-            var event = session.event;
+            var hoa = this.ensureSocketInSession(mgr, socket, args.sessionId, route);
+            var event = hoa.event;
 
-            if (!session) {
-                return mgr.writeErr(socket, route, "Session for socket not found.");
+            if (!hoa) {
+                return mgr.writeErr(socket, route, "hoa for socket not found.");
             }
-            if (!session.get("isHoA")) {
-                return mgr.writeErr(socket, route, "Session not a hangout-on-air.");
+            if (!hoa.get("isHoA")) {
+                return mgr.writeErr(socket, route, "hoa not a hangout-on-air.");
             }
             if (!socket.user.isAdminOf(event)) {
                 return mgr.writeErr(socket, route, "Not an admin");
             }
 
-            if (session.get("hangout-broadcast-id") != args["hangout-broadcast-id"]) {
-                session.save({"hangout-broadcast-id": args["hangout-broadcast-id"]});
+            if (hoa.get("hangout-broadcast-id") != args["hangout-broadcast-id"]) {
+                hoa.save({"hangout-broadcast-id": args["hangout-broadcast-id"]});
             }
             return mgr.writeAck(socket, route);
 

public/js/models.js
@@ -71,11 +71,8 @@ models.BaseModel = Backbone.Model.extend({
         this.listenTo(sub, "all", this._subEventHandlers[property])
     },
     // Unregister the events from `registerSubEvents`.
-    unregisterSubEvents: function(property) {
-        var sub = this.get(property);
-        if (sub === null) {
-            sub = this.previous(property);
-        }
+    unregisterSubEvents: function(property, sub) {
+        var sub = sub || this.get(property);
         this.stopListening(sub, "all", this._subEventHandlers[property]);
         delete this._subEventHandlers[property];
 
@@ -128,9 +125,10 @@ models.Event = models.BaseModel.extend({
         this.registerSubEvents("connectedUsers");
 
         this.on("change:hoa", _.bind(function(event, hoa) {
-            if (this.previous("hoa") && (!hoa || hoa.id != this.previous("hoa").id)) {
-                this.unregisterSubEvents("hoa");
-            } else if (!hoa.event) {
+            var prev = this.previous("hoa");
+            if (prev && (!hoa || hoa.id != prev.id)) {
+                this.unregisterSubEvents(null, prev);
+            } else if (hoa && hoa.event != this) {
                 hoa.event = this;
                 this.registerSubEvents("hoa");
             }

lib/server-models.js
@@ -145,8 +145,9 @@ exports.ServerEvent = client_models.Event.extend({
         this.initializeSubEvents();
         this.registerSubEvents("recentMessages");
         this.on("change:hoa", function(event, hoa) {
-            if (this.previous("hoa") && (!hoa || hoa.id != this.previous("hoa").id)) {
-                this.previous("hoa").destroy();
+            var prev = this.previous("hoa");
+            if (this.prev && (!hoa || hoa.id != this.prev.id)) {
+                this.prev.destroy();
             }
         }.bind(this));
         // If we get a brand new hangout broadcast ID, set it as the embeded

lib/unhangout-db.js
@@ -52,7 +52,7 @@ _.extend(UnhangoutDb.prototype, events.EventEmitter.prototype, {
 
         redis.auth(this.options.UNHANGOUT_REDIS_PASSWORD);
         redis.on("end", function() { logger.warn("redis end"); });
-        redis.on("error", function(err) { logger.error("redis error: ", err); });
+        redis.on("error", function(err) { logger.error("redis error: ", arguments); });
         redis.on("ready", function() { logger.info("redis ready"); });
         redis.once("ready", _.bind(function(err) {
             if (err) {

lib/unhangout-routes.js
@@ -159,14 +159,22 @@ module.exports = {
                             hasGooglePlusId: !!googlePlusUserId
                         })
                         var hoa = new models.ServerHoASession({});
-                        event.set("hoa", hoa); // Destroys existing hoa for event, if any.
+                        hoa.event = event;
                         hoa.markHangoutPending(req.user);
                         hoa.save({}, {
                             success: function(model) {
+                                // Set the hoa to the event, destroying a
+                                // previous hoa if any.  We don't need to save
+                                // here, because the hoa property isn't
+                                // persisted on the event model -- the
+                                // association is retrieved from the hoa's
+                                // redis URL.
+                                event.set("hoa", hoa);
                                 // If the user doesn't have any youtube channel
                                 // associated with G+, they will be prompted to
                                 // connect one, and go through mobile phone
                                 // authorization and all that jazz.
+
                                 if (process.env.NODE_ENV === "production") {
                                     return res.redirect(302,
                                         "https://plus.google.com/hangouts/_/" +

commit 2058bfd05f48c0e0d3e24e6a6b48b76bd02b0b22

Fix video URL setting broken in bc96b1350780f5991b1326bd0a1a5cf15a82af62

lib/unhangout-sockets.js
@@ -338,7 +338,6 @@ _.extend(UnhangoutSocketManager.prototype, events.EventEmitter.prototype, {
             var route = "session/set-hangout-broadcast-id";
             var hoa = this.ensureSocketInSession(mgr, socket, args.sessionId, route);
             var event = hoa.event;
-
             if (!hoa) {
                 return mgr.writeErr(socket, route, "hoa for socket not found.");
             }

public/js/models.js
@@ -130,7 +130,10 @@ models.Event = models.BaseModel.extend({
                 this.unregisterSubEvents(null, prev);
             } else if (hoa && hoa.event != this) {
                 hoa.event = this;
+            }
+            if (hoa && hoa.eventsRegisteredFor != this) {
                 this.registerSubEvents("hoa");
+                hoa.eventsRegisteredFor = this;
             }
         }, this));
         this.on("change:youtubeEmbed", _.bind(function(event, ytId) {

test/test.create-hoa.selenium.js
@@ -83,6 +83,7 @@ describe("CREATE HOA", function() {
         ).then(function(handles) {
             browser.switchTo().window(handles[1]);
         });
+        browser.waitForHangoutReady();
         browser.waitWithTimeout(function() {
             return !!event.get("hoa");
         });