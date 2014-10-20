4651ed514ef316993b53aad5f84b071332e32032
srishakatux <srishakatux@gmail.com>
2014-10-19 22:25:14 -0400

    moved the contact form to a separate file with few more additions

--- public/js/event-app.js
+++ public/js/event-app.js
@@ -184,8 +184,6 @@ $(document).ready(function() {
         $("#admin-page-for-event").attr("href", "/admin/event/" + curEvent.id);
         $(".about-event-container").hide();
 
-        $(".contact-info-modal").modal('show');
-
     }, app);
 
     app.vent.on("about-nav", _.bind(function(hide) {
