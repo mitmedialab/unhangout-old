7c90440b9efb2d3e208c0bb1fb62be4a53ca6fed
srishakatux <srishakatux@gmail.com>
2014-12-07 04:02:01 -0500

    ajax call for send email button click

--- public/js/email_preview.js
+++ public/js/email_preview.js
@@ -5,4 +5,16 @@ require([
     var curEvent = new models.ClientEvent(EVENT_ATTRS);
     var eventTitle = curEvent.get("title");
 
+    $(document).ready(function() { 
+        $("#send-email").click(function() {
+            $.ajax({
+                type: 'POST',
+                url: "/followup/event/" + curEvent.id + "/sent/",
+                data: {eventTitle: eventTitle }
+            }).fail(function(err) {
+                console.log(err);
+            });
+        });
+    });
+
 });
