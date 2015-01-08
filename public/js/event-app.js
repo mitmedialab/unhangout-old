a89efb86194258e4b77d4787068054e0fef7195b
srishakatux <srishakatux@gmail.com>
2015-01-08 13:22:24 -0500

    new files for bootstrap editable

--- public/js/event-app.js
+++ public/js/event-app.js
@@ -9,9 +9,9 @@
 
 require([
     "jquery", "underscore", "backbone", "logger", "client-models",
-    "event-views", "auth", "transport",
+    "event-views", "auth", "transport", 
     // plugins
-    "bootstrap", "backbone.marionette", "underscore-template-config"
+    "bootstrap", "bootstrap-editable", "backbone.marionette", "underscore-template-config"
 ], function($, _, Backbone, logging, models, eventViews, auth, transport) {
 
 var curEvent, messages;
@@ -183,6 +183,7 @@ $(document).ready(function() {
 
         $("#admin-page-for-event").attr("href", "/admin/event/" + curEvent.id);
         $(".about-event-container").hide();
+
 
     }, app);
 
