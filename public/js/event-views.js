6f1d6c6903dc9ec13df0c0a5291fd918a411369f
hyun94 <hyun94@mit.edu>
2015-01-13 11:45:38 -0500

    commented out check for proposeSession

--- public/js/event-views.js
+++ public/js/event-views.js
@@ -351,25 +351,26 @@ views.DialogView = Backbone.Marionette.Layout.extend({
         $(".proposed-title-validate-error").hide();
         $(".propose-session-template").css("min-height", "115px");
 
+        var title = $("#sessionTitle").text();
         var participantName = $("#participantName").text();
 
-        if(participantName.length > 20) {
-            $(".proposed-title-validate-error").show();
-            $(".propose-session-template").css("min-height", "145px");
-            scope.modal('show');
-            return;
-        }
+        // if(participantName.length > 20) {
+        //     $(".proposed-title-validate-error").show();
+        //     $(".propose-session-template").css("min-height", "145px");
+        //     scope.modal('show');
+        //     return;
+        // }
 
         // force sessions type to be "simple"
-        // var activities = [];
-        // activities.push({type: "about", autoHide: true});
+        var activities = [];
+        activities.push({type: "about", autoHide: true});
 
-        // this.options.transport.send("create-session", {
-        //     title: "",
-        //     description:"",
-        //     activities: activities,
-        //     roomId: this.options.event.getRoomId()
-        // });
+        this.options.transport.send("create-session", {
+            title: title,
+            description:"",
+            activities: activities,
+            roomId: this.options.event.getRoomId()
+        });
     },
 
     closeDisconnected: function() {
