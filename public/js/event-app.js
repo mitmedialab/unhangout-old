f90e0bd749964cee71640183ef791fee86696f6f
srishakatux <srishakatux@gmail.com>
2015-02-09 16:43:12 -0500

    equal value and equal type javascript operator

--- public/js/event-app.js
+++ public/js/event-app.js
@@ -191,9 +191,9 @@ $(document).ready(function() {
         $(".about-event-container").hide();
 
         //Show contact form on event page if either 
-        //preferred contact is null or noShare is false 
+        //preferred contact is false or noShare is false 
 
-        if(preferredContact == false && (noShare == false || noShare == undefined)) {
+        if(preferredContact === false && (noShare === false || noShare === undefined)) {
             $("#my-contact-info-modal").modal('show');
         }
 
