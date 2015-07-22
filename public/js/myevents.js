c79551e3b6e0444082dbafe594b0d2a1f6b541c9
srishakatux <srishakatux@gmail.com>
2015-07-22 15:19:29 -0400

    backbone view bare minimum for my events

--- public/js/myevents.js
+++ public/js/myevents.js
@@ -1,4 +1,21 @@
-require(["jquery", "bootstrap", "auth"], function($) {
-    $("[rel=popover]").popover({container: "body", placement: "left"});
-    $("[title]").not("[rel=popover]").tooltip({container: "body"});
+require([
+   "jquery", "underscore", "backbone", "client-models", "auth",
+   // plugins
+   "backbone.marionette", "bootstrap", "underscore-template-config"
+], function($, _, Backbone, models, auth) {
+
+$(document).ready(function() { 
+
+	var users = new models.UserList(USER_DATA);
+	var events = new models.EventList(EVENT_DATA);
+
+	var AdminEventRowView = Backbone.Marionette.ItemView.extend({
+
+	});
+
+});
+
+$("[rel=popover]").popover({container: "body", placement: "left"});
+$("[title]").not("[rel=popover]").tooltip({container: "body"});
+
 });
