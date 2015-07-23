e2928dc0b9fec0123a98979ba1c77f4aea74e6c5
srishakatux <srishakatux@gmail.com>
2015-07-22 20:32:32 -0400

    backbone event table and row view setup for  admin's events

--- public/js/myevents.js
+++ public/js/myevents.js
@@ -9,11 +9,51 @@ $(document).ready(function() {
 	var users = new models.UserList(USER_DATA);
 	var events = new models.EventList(EVENT_DATA);
 
-	var AdminEventRowView = Backbone.Marionette.ItemView.extend({
+	var EventRowView = Backbone.Marionette.ItemView.extend({
+	    tagName: 'tr',
+	    template: '#event-row', 
+    });
+
+	var EventTableView = Backbone.Marionette.CompositeView.extend({
+
+	    template: '#event-table',
+	    itemView: EventRowView,
+	    itemViewContainer: 'tbody',
+
+	    initialize: function(options) {
+	    	this.collection = events; 
+	    },
+
+	    serializeData: function() {
+	    	var context = Backbone.Marionette.CompositeView.prototype.serializeData.apply(this);
+	        
+	        context.adminEvents = [];
+
+	        events.each(function(event) {
+	 			context.adminEvents.push(event.toJSON());
+	        });
+
+	        return context;
+	    },
 
 	});
 
-});
+	/* -------------------   App initialization ---------------- */
+
+	var app = new Backbone.Marionette.Application();
+
+	app.addRegions({
+	    main: "#main"
+	});
+
+	app.addInitializer(function(options) {
+	    var eventTable = new EventTableView({model: events});
+	    this.main.show(eventTable);
+	});
+
+	app.start();
+
+}); //document ready 
 
 $("[rel=popover]").popover({container: "body", placement: "left"});
 $("[title]").not("[rel=popover]").tooltip({container: "body"});
