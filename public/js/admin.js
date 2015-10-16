ec36ee7e1eef60ce23713fe7771eb8693a01fd17
Chad Phillips <chad@apartmentlines.com>
2015-10-16 10:57:39 -0500

    simple event deletion interface.
    Adds a delete button to all events on the /admin/ page, with a simple
    confirmation dialog. A further server side check is done to ensure
    no users are actively connected to the event prior to deletion. The
    event and all associated sessions are deleted as part of this action.
    
    Used the node flash module for simple user feedback.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,9 @@
 require(["jquery", "bootstrap", "auth"], function($) {
     $("[rel=popover]").popover({container: "body", placement: "left"});
     $("[title]").not("[rel=popover]").tooltip({container: "body"});
+    $(".delete-event").click(function(event) {
+      var id = $(this).attr("data-event");
+      var deleteEvent = confirm("Are you sure you want to delete event " + id + " and all associated sessions? This cannot be undone.");
+      return deleteEvent;
+    });
 });
