9431ca4befc85c224516366266d9e16ff4d67ce9
srishakatux <srishakatux@gmail.com>
2016-04-18 18:43:35 -0400

    CLEANUP remove connected users from session view

--- public/js/event-app.js
+++ public/js/event-app.js
@@ -125,7 +125,6 @@ $(document).ready(function() {
         this.sessionListView = new eventViews.SessionListView({
             collection: curEvent.get("sessions"),
             event: curEvent,
-            users: curEvent.get("connectedUsers"),
             transport: trans
         });
         this.topicListView = new eventViews.TopicListView({
