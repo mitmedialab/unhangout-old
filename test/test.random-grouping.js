316798244beeccaa884d1478cf3a8d30f1b08956
Charlie DeTar <cfd@media.mit.edu>
2016-04-20 17:15:48 -0600

    Add initial random-grouping library tests, fix logging

--- test/test.random-grouping.js
+++ test/test.random-grouping.js
@@ -1,42 +1,130 @@
-var server = require("../lib/unhangout-server"),
-    expect = require("expect.js"),
-    _ = require("underscore"),
-    models = require("../lib/server-models"),
-    options = require("../lib/options"),
-    groupBooter = require("../lib/random-grouping"),
-    request = require('superagent');
-    common = require("./common");
+var server = require("../lib/unhangout-server");
+var expect = require("chai").expect;
+var _ = require("underscore");
+var models = require("../lib/server-models");
+var options = require("../lib/options");
+var randomGrouping = require("../lib/random-grouping");
+var request = require('superagent');
+var common = require("./common");
+var Promise = require("bluebird");
 
-describe("GROUP USER", function() {
-	var event, eventId, group;
+describe("RANDOM GROUPING", function() {
+  var event, eventId, group;
 
-    beforeEach(function(done) {
-	    common.standardSetup(function() {
-	      event = common.server.db.events.get(1);
-	      event.set("randomizedSessions", true);
-	      eventId = event.get("id");
-	      group = groupBooter(common.server.db, options);
-	      done();
-	    });
-  	});
-
-    afterEach(common.standardShutdown);
-    after(common.restoreTimers);
-
-    it("GROUP ASSIGNED", function(done) {
-    	var user = common.server.db.users.findWhere({"sock-key": "regular1"});
-    	user.setSessionPreference(eventId, null);
-    	var userId = user.get("id");
-
-    	request.get(common.URL + "/event/" + eventId)
-            .set("x-mock-user", "regular1")
-            .redirects(0)
-            .end(function(res) {
-            	group.assignGroupToUser(userId, eventId).then(function() {
-            		//TODO
-            	}); 
-                expect(res.status).to.be(200);
-                done();
-            });
+  beforeEach(function(done) {
+    common.standardSetup(function() {
+      event = common.server.db.events.get(1);
+      event.set("randomizedSessions", true);
+      eventId = event.get("id");
+      group = randomGrouping(common.server.db, options);
+      done();
     });
+  });
+
+  afterEach(common.standardShutdown);
+
+  describe("library", function() {
+    it("Creates randomized session", function() {
+      var length = event.get("sessions").length;
+      var sess1, sess2;
+
+      expect(length).to.be.above(0);
+      expect(event.getRandomizedSessions()).to.deep.equal([]);
+
+      return group._createRandomizedSession(event)
+      .then(function(session) {
+        sess1 = session;
+        expect(event.get("sessions").length).to.equal(length + 1);
+        expect(event.get("sessions").at(length)).to.equal(session);
+
+        expect(session.get('title')).to.equal("Breakout Room 1");
+        expect(session.get('proposedBy')).to.be.null;
+        expect(session.get('activities')).to.deep.equal([{type: "about", autoHide: true}]);
+        expect(session.get('joinCap')).to.equal(6);
+        expect(session.get('randomized')).to.equal(true);
+        expect(session.get('approved')).to.equal(true);
+
+        return group._createRandomizedSession(event);
+      })
+      .then(function(session) {
+        sess2 = session;
+        expect(event.get("sessions").length).to.equal(length + 2);
+        expect(session.get("title")).to.equal("Breakout Room 2");
+        expect(session.get('randomized')).to.be.true;
+      })
+      .then(function() {
+        expect(event.getRandomizedSessions()).to.deep.equal([sess1, sess2]);
+      });
+    });
+
+    it("Assigns a user to a session", function() {
+      var user = common.server.db.users.at(0);
+      return group.assignRandomizedSession(event.id, user.id)
+      .then(function(sessionId) {
+        var session = event.get("sessions").get(sessionId);
+        expect(session).to.not.be.null;
+        expect(session.get("assignedParticipants")).to.deep.equal([user.id]);
+        var pref = {};
+        pref[event.id] = session.id;
+        expect(user.get("sessionPreference")).to.deep.equal(pref);
+        // Reassignment
+        return group.assignRandomizedSession(event.id, user.id).then(function(sessionId2) {
+          expect(sessionId2).to.not.equal(sessionId);
+          var session2 = event.get("sessions").get(sessionId2);
+          expect(session.get("assignedParticipants")).to.deep.equal([]);
+          expect(session2.get("assignedParticipants")).to.deep.equal([user.id]);
+          pref[event.id] = session2.id;
+          expect(user.get("sessionPreference")).to.deep.equal(pref);
+        });
+      });
+    });
+
+    it("Mass asignment: multiple sessions", function() {
+      var p = Promise.resolve();
+      _.range(13).map(function(i) {
+        p = p.then(function() {
+          var user = new models.ServerUser(
+            {displayName: "Rando " + i},
+            {collection: common.server.db.users}
+          );
+          return new Promise(function(resolve, reject) {
+            user.save({}, {
+              success: function(user) {
+                user.collection.add(user);
+                resolve(user);
+              },
+              error: reject,
+            });
+          }).then(function(user) {
+            return group.assignRandomizedSession(event.id, user.id);
+          });
+        });
+      })
+      return p
+      .then(function() {
+        var randos = event.getRandomizedSessions();
+        expect(randos.length).to.equal(3);
+        expect(randos[0].get("assignedParticipants").length).to.equal(6);
+        expect(randos[1].get("assignedParticipants").length).to.equal(6);
+        expect(randos[2].get("assignedParticipants").length).to.equal(1);
+      });
+    });
+  });
+
+//  it("", function(done) {
+//    var user = common.server.db.users.findWhere({"sock-key": "regular1"});
+//    user.setSessionPreference(eventId, null);
+//    var userId = user.get("id");
+//
+//    request.get(common.URL + "/event/" + eventId)
+//      .set("x-mock-user", "regular1")
+//      .redirects(0)
+//      .end(function(res) {
+//        group.assignGroupToUser(userId, eventId).then(function() {
+//          //TODO
+//        });
+//        expect(res.status).to.be(200);
+//        done();
+//      });
+//  });
 });
