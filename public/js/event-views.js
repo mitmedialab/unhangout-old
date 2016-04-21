465479c8b3079cff46e5a9cb9358abfd6a4df6d3
srishakatux <srishakatux@gmail.com>
2016-04-20 21:51:34 -0400

    render session list view on remove too

--- public/js/event-views.js
+++ public/js/event-views.js
@@ -552,6 +552,7 @@ views.SessionListView = Backbone.Marionette.CollectionView.extend({
         this.listenTo(this.options.event, 'change:adminProposedSessions', this.render, this);
         this.listenTo(this.options.event, 'change:randomizedSessions', this.render, this);
         this.listenTo(this.options.event.get("sessions"), 'change:assignedParticipants', this.render, this);
+        this.listenTo(this.options.event.get("sessions"), 'remove', this.render, this);
     },
 
     itemViewOptions: function() {        
